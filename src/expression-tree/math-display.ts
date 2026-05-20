ebf979eb8b13c3bf98e4590a450e71a7d40faac7
someone <chowchunhei9@gmail.com>
2026-05-20 20:08:49 +0800

    math display PARENs mostly working

--- src/expression-tree/math-display.ts
+++ src/expression-tree/math-display.ts
@@ -2,47 +2,58 @@ import { type SExpression  } from "./parser";
 import { getInfixBP, getPrefixBP, getPostfixBP, PREFIX_BINDING_POWERS } from "./binding-powers";
 
 export function displayS(sNode: SExpression): string {
-    function displayHelper(sNode: SExpression): [String, string | null] { // [Sstring, op]
+    // SExpression -> [Sstring, op, 'pre'|'in'|'post']
+    function displayHelper(sNode: SExpression): [string, string | null, ('pre'|'in'|'post') | null] { 
         if ( sNode.type === 'Atom' || sNode.rest.length === 0){
-            return [sNode.value, null];
+            return [sNode.value, null, null];
         }
         const op = sNode.value;
         if (sNode.rest.length > 1){   // More than one child means infix operator
             const parentBP = getInfixBP(op);
             const result = sNode.rest.map( (s) => { 
-                let [sString, childOp] = displayHelper(s);
+                let [sString, childOp, childFix] = displayHelper(s);
                 // Fallback wrap in brackets in case of some unknown op
                 if (parentBP === null) { return '(' + sString + ')'; }
                 // Atoms don't need brackets
                 if (childOp === null) return sString; 
                 // If child is prefix with larger BP then no brackets around it
                 // eg. (+ a (* b c))  => a + b*c ;  (+ a (- b))  => a + -b 
-                if ( childOp in PREFIX_BINDING_POWERS && getPrefixBP(childOp)! > parentBP[0]){
+                if ( childFix === 'pre' && getPrefixBP(childOp)! > parentBP[0]
+                  || childFix === 'post' && getPostfixBP(childOp)! > parentBP[0]){
                      return sString; 
                 }
                 const childBP = getInfixBP(childOp);
                 if (childBP === null) { 
                     return '(' + sString + ')'; }
                 // Only add brackets when child binding power is weaker than parent 
-                if ( childBP > parentBP ){
-                    return sString;
-                }
+                if ( childBP[0] > parentBP[0] ){
+                    return sString; }
                 return '(' + sString + ')';
             } ).join(op);
-            return [ result, op];
+            return [ result, op, 'in'];
         }
         // Unary operator
-        let [sString, _] = displayHelper(sNode.rest[0]);
-        if (op === '-' && sNode.rest[0].type === 'Atom'){ // Don't add brackets for -Atom
-            return [ op + sString, op]; } 
+        let [sString, cOp, cFix] = displayHelper(sNode.rest[0]);
+        if (op === '-'){
+            if (cOp === null) return ['-' + sString, op, 'pre'];
+            if (cFix === 'in'){
+                let cBp = getInfixBP(cOp);
+                if (cBp === null) return [`-(${sString})`, op, 'pre'];
+                if (cBp[0] > getPrefixBP('-')!)  return ['-' + sString, op, 'pre'];
+                [`-(${sString})`, op, 'pre'];
+            }
+            return ['-' + sString, op, 'pre'];
+        }
         else if (op === '√' ){
-            return [ `\\sqrt{${sString}}`, op]; }
+            return [ `\\sqrt{${sString}}`, op, 'pre']; }
         else if (op === 'inv' ){  //  a^(-1)
-            return [ `\\frac{1}{${sString}}`, op]; }
+            return [ `\\frac{1}{${sString}}`, op, 'pre']; }
+        else if (op === '!' ){  //  a^(-1)
+            return [ `${sString}!`, op, 'post']; }
         // Other Prefix functions
-        return [ `${op}(${sString})`, op];
+        return [ `${op}(${sString})`, op, 'pre'];
     }     
-    let [result, _] = displayHelper(sNode);
+    let [result, _1, _2] = displayHelper(sNode);
     result = result.replaceAll('+-', '-');
     console.log("res:", result)
     return '$$' + result + '$$';
