0d1a083f694793db96cf0ce7e936d1fe8941047f
someone <chowchunhei9@gmail.com>
2026-05-20 13:50:32 +0800

    math display handles redundant PARENs

--- src/expression-tree/math-display.ts
+++ src/expression-tree/math-display.ts
@@ -1,12 +1,22 @@
 import { type SExpression  } from "./parser";
+import { getInfixBP, getPrefixBP, getPostfixBP } from "./binding-powers";
 
 export function displayS(sNode: SExpression): string {
-    function displayHelper(sNode: SExpression): String {
+    function displayHelper(sNode: SExpression): [String, string | null] { // [Sstring, op]
         if ( sNode.type === 'Atom'){
-            return sNode.value;
+            return [sNode.value, null];
         }
         const op = sNode.value;
-        return sNode.rest.map( s => displayHelper(s)  ).join(op);
+        const result = sNode.rest.map( (s) => { 
+            let [sString, childOp] = displayHelper(s);
+            if (childOp === null) return sString;
+            if ( getInfixBP(childOp) > getInfixBP(op) ){
+                return sString;
+            }
+            return '(' + sString + ')';
+        } ).join(op);
+        return [ result, op];
         }
-    return '$$' + displayHelper(sNode) + '$$';
+    const [result, _] = displayHelper(sNode);
+    return '$$' + result + '$$';
 }
